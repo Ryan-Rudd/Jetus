40182ef768da169f49bd7e8f695e7ca2dc21ef93
Tech Penguin <131509539+Ryan-Rudd@users.noreply.github.com>
2023-05-21 15:12:49 -0400

    Update Main.cpp

--- jetus/Main.cpp
+++ jetus/Main.cpp
@@ -7,4 +7,7 @@ string INPUT;
 int main()
 {
     cin >> INPUT;
+    cout << "INPUT MEMORY REFFERENCE: \t" <<  (&INPUT) << endl;
+    cout << "INPUT MEMORY ADDRESS:\t\t" << static_cast<void*>(&INPUT) << endl;
+
 }
